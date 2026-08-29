f0c69098bca08a16349c34adeb0ebd80e69cb09e
Arthur <arthur.felipe1993@gmail.com>
2026-08-29 03:56:34 -0300

    OKAN-031: expand workouts repository for weekly plans

--- lib/features/workouts/domain/repositories/workouts_repository.dart
+++ lib/features/workouts/domain/repositories/workouts_repository.dart
@@ -1,3 +1,4 @@
+import '../entities/weekly_workout_plan.dart';
 import '../entities/workout_exercise.dart';
 import '../entities/workout_history.dart';
 import '../entities/workout_model.dart';
@@ -22,11 +23,60 @@ abstract class WorkoutsRepository {
     required String dayKey,
   });
 
+  Stream<WeeklyWorkoutPlan> watchWeeklyPlan(String studentId);
+
+  Future<void> saveWorkoutDay({
+    required String studentId,
+    required String dayKey,
+    required List<WorkoutExercise> exercises,
+  });
+
+  Future<void> saveWorkoutFeedback({
+    required String studentId,
+    required String dayKey,
+    required String feedback,
+  });
+
+  Future<void> clearWorkoutFeedback({
+    required String studentId,
+    required String dayKey,
+  });
+
+  Future<void> setWorkoutValidity({
+    required String studentId,
+    required DateTime validade,
+  });
+
+  Future<void> markExpiryWarningSent(String studentId);
+
+  Future<bool> isTrainingProfessional(String userId);
+
+  Future<String?> findStudentProfessionalId(String studentId);
+
+  Future<void> notifyUser({
+    required String userId,
+    required String type,
+    required String title,
+    required String body,
+    required String actionId,
+  });
+
+  Stream<List<WorkoutTemplate>> watchWorkoutTemplates(String personalId);
+
+  Future<void> saveWorkoutTemplate({
+    required String personalId,
+    required String nome,
+    required List<WorkoutExercise> exercises,
+  });
+
+  Future<void> deleteWorkoutTemplate(String templateId);
+
   Future<void> completeWorkoutDay({
     required String studentId,
     required String dayKey,
     required List<WorkoutExercise> exercises,
     required String feedback,
+    bool clearPlanFeedback = false,
   });
 
   Stream<List<WorkoutHistory>> watchWorkoutHistory(String studentId);
