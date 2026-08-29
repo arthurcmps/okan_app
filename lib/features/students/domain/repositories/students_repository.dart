6e3e3188061ea7462651260e2444f823882cbe96
Arthur <arthur.felipe1993@gmail.com>
2026-08-29 11:56:48 -0300

    OKAN-032: expose student profile through repository

--- lib/features/students/domain/repositories/students_repository.dart
+++ lib/features/students/domain/repositories/students_repository.dart
@@ -1,11 +1,14 @@
 import '../entities/pending_student_invite.dart';
 import '../entities/student_invite_creation_result.dart';
+import '../entities/student_profile.dart';
 import '../entities/student_summary.dart';
 
 abstract interface class StudentsRepository {
   Stream<bool> watchProfessionalPremium(String professionalId);
 
   Stream<List<StudentSummary>> watchActiveStudents(String professionalId);
+
+  Stream<StudentProfile?> watchStudentProfile(String studentId);
 
   Future<List<StudentSummary>> findCanonicalStudentsByEmail(String email);
 
